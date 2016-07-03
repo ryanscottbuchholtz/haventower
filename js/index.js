0d78b4e00971c0898a2ad193fd7022bf22f593de
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-07-03 13:37:57 -0700

    initial fadein

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 require('../css/style.css');
+
+var $ = require('jquery');
+
+$(document).ready(function() {
+  $('#main-image-logo-opacity-wrapper').delay(2000).fadeOut(4000);
+  $('#main-image-logo-wrapper').delay(2000).fadeOut(4000);
+  $('.main-header').fadeIn(8000).css('display', 'flex');
+})
