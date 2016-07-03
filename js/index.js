ad7ae83650b9a8658658719e745bbc7a0a04f9b2
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-07-03 15:14:39 -0700

    learn more display fade fix

--- js/index.js
+++ js/index.js
@@ -5,5 +5,5 @@ var $ = require('jquery');
 $(document).ready(function() {
   $('#main-image-logo-opacity-wrapper').delay(2000).fadeOut(4000);
   $('#main-image-logo-wrapper').delay(2000).fadeOut(4000);
-  $('.main-header').fadeIn(8000).css('display', 'flex');
+  $('.main-header, .learn-more').fadeIn(7000).css('display', 'flex');
 })
