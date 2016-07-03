3117762b4881bf55554eebf722ef7a8d89e9fde4
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-07-03 16:41:17 -0700

    about us copy added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-require('../css/style.css');
+require("../css/style.css");
+require("font-awesome-webpack");
 
 var $ = require('jquery');
 
 $(document).ready(function() {
   $('#main-image-logo-opacity-wrapper').delay(2000).fadeOut(4000);
   $('#main-image-logo-wrapper').delay(2000).fadeOut(4000);
-  $('.main-header, .learn-more').fadeIn(7000).css('display', 'flex');
+  $('.main-header, .learn-more').fadeIn(6000).css('display', 'flex');
 })
--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,11 @@ module.exports = {
           test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader", "file-loader")
         },
         { test: /\.(jpe?g|png|git|svg)$/, loader: "file-loader"
-        }
+        },
+        {   test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"
+          },
+        {   test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"
+          }
       ]
     }
 
